c3db47d6c07fc4b406323c5827d765889ee5a2e8
KimMaru10 <maruuchi@pencil.co.jp>
2025-03-19 18:44:18 +0900

    [#1]init: 基本的なcmsの追加

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,12 +3,47 @@
 
 namespace App\Model\Table;
 
+//Table クラス
 use Cake\ORM\Table;
+
+//Text クラス
+use Cake\Utility\Text;
+
+//EventInterface　クラス
+use Cake\Event\EventInterface;
+
+// Validator クラス
+use Cake\Validation\Validator;
+
 
 class ArticlesTable extends Table
 {
-    public function initialise(array $config) : void
+    public function initialize(array $config) : void
     {
+        parent::initialize($config);
+        
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave(EventInterface $event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // スラグをスキーマで定義されている最大長に調整
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->notEmptyString('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmptyString('body')
+            ->minLength('body', 10);
+
+        return $validator;
     }
 }
